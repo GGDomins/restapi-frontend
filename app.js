dd71d1a2a3090fc4c0f77a267023be9319c1dd10
sway3 <svvoo31o@gmail.com>
2023-05-20 20:31:51 +0900

    [!HOTFIX] port change

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+app.listen(process.env.PORT || 5000, () => {
+  console.log('hello');
 });
     
