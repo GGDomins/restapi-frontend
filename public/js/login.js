f52d78636897a2e9a1b1a902f31020046b90f74b
sway3 <svvoo31o@gmail.com>
2023-05-23 17:52:08 +0900

    [Fix] token error in index.js

--- public/js/login.js
+++ public/js/login.js
@@ -3,6 +3,7 @@ const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
 
 function loginRequest(event) {
+
     const email = emailInput.value;
     const password = passwordInput.value;
 
@@ -24,42 +25,43 @@ function loginRequest(event) {
             const statusCode = response.status;
 
             if (statusCode === 200) {
-                return response.json();
+                const accessToken = response.headers.get('accessToken');
+                const expireTime = response.headers.get('expireTime');
+
+                console.log(accessToken);
+
+                localStorage.setItem('accessToken', accessToken);
+                localStorage.setItem('expireTime', expireTime);
+
+                console.log('200 OK / Login Successful');
+                alert('로그인 성공!');
+
+                window.location.href = "index.html";
             } else if (statusCode === 401) {
-                return response.json().then(data => {
-                    const message = data.message; // Use 'data' instead of 'response'
+                return response.json().then(response => {
+                    const message = response.json().message;
                     alert(message);
                     console.log('401 Unauthorized');
 
-                    if (message === "이메일을 잘못 입력하셨습니다.") {
+                    if (message == "이메일을 잘못 입력하셨습니다.") {
                         emailInput.focus();
                     } else {
                         passwordInput.focus();
                     }
                     throw new Error('401 Unauthorized');
-                });
+                })
             } else {
                 throw new Error('Unexpected error');
             }
-        })
-        .then(data => {
-            const accessToken = response.headers.get('accessToken');
-            const expireTime = response.headers.get('expireTime');
 
-            console.log(accessToken);
-
-            localStorage.setItem('accessToken', accessToken);
-            localStorage.setItem('expireTime', expireTime);
-
-            console.log('200 OK / Login Successful');
-            alert('로그인 성공!');
-
-            window.location.href = "index.html";
+            // for (const [name, value] of response.headers.entries()) {
+            //     console.log(`${name}: ${value}`);
+            // }
         })
         .catch(error => {
             console.error(error);
         });
-
+    
     event.preventDefault();
 }
 
