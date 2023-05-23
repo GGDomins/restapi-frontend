c6b873f433ea6b8b4276ebfba224daff3b7af65a
sway3 <svvoo31o@gmail.com>
2023-05-23 17:33:18 +0900

    [Fix] token error in index.js

--- public/js/login.js
+++ public/js/login.js
@@ -3,7 +3,6 @@ const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
 
 function loginRequest(event) {
-
     const email = emailInput.value;
     const password = passwordInput.value;
 
@@ -27,29 +26,25 @@ function loginRequest(event) {
             if (statusCode === 200) {
                 return response.json();
             } else if (statusCode === 401) {
-                return response.json().then(response => {
-                    const message = response.json().message;
+                return response.json().then(data => {
+                    const message = data.message; // Use 'data' instead of 'response'
                     alert(message);
                     console.log('401 Unauthorized');
 
-                    if (message == "이메일을 잘못 입력하셨습니다.") {
+                    if (message === "이메일을 잘못 입력하셨습니다.") {
                         emailInput.focus();
                     } else {
                         passwordInput.focus();
                     }
                     throw new Error('401 Unauthorized');
-                })
+                });
             } else {
                 throw new Error('Unexpected error');
             }
-
-            // for (const [name, value] of response.headers.entries()) {
-            //     console.log(`${name}: ${value}`);
-            // }
         })
         .then(data => {
-            const accessToken = data.headers.get('accessToken');
-            const expireTime = data.headers.get('expireTime');
+            const accessToken = response.headers.get('accessToken');
+            const expireTime = response.headers.get('expireTime');
 
             console.log(accessToken);
 
@@ -60,13 +55,11 @@ function loginRequest(event) {
             alert('로그인 성공!');
 
             window.location.href = "index.html";
-
-            //return response.json();
         })
         .catch(error => {
             console.error(error);
         });
-    
+
     event.preventDefault();
 }
 
